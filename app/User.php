8b3f9a6599dd8b7ea0aff1291646d10b3dad4cd1
Rosiliza Rioja <rrioja@yondu.com>
2016-07-23 18:08:41 +0800

    User model: add guarded fields

--- app/User.php
+++ app/User.php
@@ -28,7 +28,7 @@ class User extends Model implements AuthenticatableContract,
      *
      * @var array
      */
-    protected $fillable = ['name', 'email', 'password'];
+    protected $guarded = ['id', 'created_at', 'updated_at'];
 
     /**
      * The attributes excluded from the model's JSON form.
